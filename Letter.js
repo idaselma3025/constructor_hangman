ed1951f79c9ce725cb359425603067d1c5198a33
Craig Christensen <ruffcorn@gmail.com>
2018-02-11 10:28:42 -0600

    More changes to the constructor in Letter.js

--- Letter.js
+++ Letter.js
@@ -1,11 +1,22 @@
-// Letter is a constructor function that take a letter as input and make
-// a Letter object.  The Boolean Letter.visible determines if Letter.letter will
-// be shown or a masking character '_' will be shown.  That happens in 
-// the Letter.returnChar() method.
+// Letter is a constructor function that take a letter as input and makes
+// a Letter object.  The Boolean Letter.visible is a switch that is used
+// to control wheter Letter.letter or a masking character '_' will be returned.
 
 var Letter = function(letter){
   this.letter = letter;
   this.visible = false;
+
+  // The method Letter.compareChar takes a string 'ltr' as input.  It compares 
+  // ltr to Letter.letter.  If they are the same - if the user guessed correctly
+  // then Letter.visible is set to true.
+  this.compareChar = function(ltr){
+    if (this.ltr === this.letter){
+      this.visible = true;
+    };
+  };
+
+  // The method Letter.returnChar uses the visible boolean value to determine 
+  // whether to return Letter.letter or an underscore character.
   this.returnChar = function(){
     if (this.visible) {
       return this.letter;
@@ -14,6 +25,7 @@ var Letter = function(letter){
       return '_';
     };
   };
+
 };
 
 module.exports = Letter;
